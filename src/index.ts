117e1a526f152780a449749cbf7fd92743d38bf7
vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-26 14:02:56 +0200

    advancing more

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,35 @@
-import Slack, {
-} from "@slack/bolt";
-
-const {App, subtype} = Slack;
+import Slack, {} from "@slack/bolt";
+const { App, subtype } = Slack;
+import SlackOauth from "@slack/oauth";
+const { FileInstallationStore } = SlackOauth;
 
 import { config } from "dotenv";
-import { Models } from "./db/db.js";
 import { Sequelize } from "sequelize";
+import { Models } from "./db/db.js";
 config();
 
-
-const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, SLACK_APP_TOKEN } = process.env;
+const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, SLACK_APP_TOKEN,SLACK_CLIENT_SECRET } = process.env;
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'db.sqlite',
+	dialect: "sqlite",
+	storage: "db.sqlite",
 });
-const {User} = await Models(sequelize);
+const { User } = await Models(sequelize);
 
 const slack = new App({
 	token: SLACK_BOT_TOKEN,
 	appToken: SLACK_APP_TOKEN,
 	socketMode: true, //TODO dont use socket mode
 	signingSecret: SLACK_SIGNING_SECRET,
-	port: 6777
+	port: 6777,
+	clientSecret: SLACK_CLIENT_SECRET,
+	installationStore: new FileInstallationStore(),
+});
+
+
+slack.command("/ooinvite", async ({ ack, body, client }) => {
+	await ack();
+	console.log(body);
+	const { user_id } = body;
+	
 });
