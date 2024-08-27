4137d4adebadcbb99344e52ab66fff2741e3f8e8
vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-27 17:27:26 +0200

    chat?

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import { Models } from "./db/db.js";
 config();
 
 const { SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET, SLACK_APP_TOKEN,SLACK_CLIENT_SECRET } = process.env;
+
+const ONLY_ONCE_META = "C07JQF74HQT";
 const ONLY_ONCE_CHANNEL = "C07K5SSA908";
 const ONLY_ONCE_PORTAL = "C07JGDNQ91U";
 
@@ -28,11 +30,11 @@ const slack = new App({
 });
 
 
-slack.event("member_joined_channel", async({client,body,})=>{
+slack.event("member_joined_channel", async({client,body,say})=>{
 
-	console.log("Recieved member_joined_channel event",body);
-
+	
 	if (body.channel !== ONLY_ONCE_PORTAL) return;
+	console.log("Recieved member_joined_channel event",body);
 	const {user} = body;
 
 	const invite_message = {
@@ -103,10 +105,11 @@ slack.event("member_joined_channel", async({client,body,})=>{
 			}
 		]
 	}
-	client.chat.postMessage({
-		channel: body.channel,
+
+	slack.client.chat.postMessage({
+		channel: ONLY_ONCE_CHANNEL,
 		...invite_message
-	})
+	}).catch(console.error);
 })
 
 
@@ -116,7 +119,7 @@ slack.message("debug_channel_join", async({client,body,message,say})=>{
 	console.log("Recieved [DEBUG] member_joined_channel message",message);
 
 
-	if (body.channel_id !== ONLY_ONCE_PORTAL) return;
+	if (message.channel !== ONLY_ONCE_PORTAL) return;
 	const user = message.user;
 	const invite_message = {
 		"text":"Welcome to the Only Once portal. Please read this on the slack client.",
@@ -133,7 +136,7 @@ slack.message("debug_channel_join", async({client,body,message,say})=>{
 				"type": "section",
 				"text": {
 					"type": "mrkdwn",
-					"text": `Welcome to the Only Once portal, ${user}. From here you can access the Only Once channel.`
+					"text": `Welcome to the Only Once portal, <@${user}>. From here you can access the Only Once channel.`
 				}
 			},
 			{
@@ -150,7 +153,7 @@ slack.message("debug_channel_join", async({client,body,message,say})=>{
 				"type": "section",
 				"text": {
 					"type": "mrkdwn",
-					"text": "*RULES*\n\n- If you send the same message as anyone before you'll get *BANNED*.\n- The max text length is 300characters. If you use more you'll get *BANNED*. \n- If you spam random characters to abuse the system you'll get *BANNED*."
+					"text": "*RULES*\n\n- If you send the same message as anyone before you'll get *BANNED*.\n- The max text length is 300characters. If you use more you'll get *BANNED*. \n- If you spam random characters to abuse the system you'll get *BANNED*.\n- All your messages sent in #only-once will be stored in a database unencrypted and linked with your user id."
 				}
 			},
 			{
@@ -197,8 +200,8 @@ slack.message("debug_channel_join", async({client,body,message,say})=>{
 slack.action("joinonlyonce", async({client,body,ack})=>{
 	const user = body.user.id;
 
-	const u = await User.findOne({where:{id:user}});
-	if (!u){
+	let u = await User.findOne({where:{id:user}});
+	if (u){
 
 		await client.chat.postEphemeral({
 			text:"You already have access to the Only Once channel or have been banned!!",
@@ -207,13 +210,22 @@ slack.action("joinonlyonce", async({client,body,ack})=>{
 		})
 	}
 
-	await User.create({id:user,banned:false,messageCount:0});
+	u = await User.create({id:user,banned:false,messageCount:0});
 
 	await client.chat.postMessage({
 		text:"There you go, youre being added to the Only Once channel",
 		channel: ONLY_ONCE_PORTAL
 	})
 
+	await u.save().catch(slackLog);
+
+	await client.conversations.invite({
+		channel: ONLY_ONCE_CHANNEL,
+		users: user
+	}).catch(slackLog);
+
+	slackLog(`Added <@${user}> to the Only Once channel`);
+	
 })
 
 slack.command("/ooinvite", async ({ ack, body, client,respond }) => {
@@ -272,35 +284,87 @@ slack.command("/oorm", async ({ ack, body, client,respond }) => {
 	}
 })
 
+slack.command("/ooupdatecanvas", async ({ ack, body, client,respond }) => {
+	const data = await User.findAll();
+
+	const canvas = data.map((u) => {
+		return {
+			type: "section",
+			text: {
+				type: "mrkdwn",
+				text: `<@${u.dataValues.id}> has sent ${u.dataValues.messageCount} messages and is ${u.dataValues.banned ? "banned" : "not banned"}`
+			}
+		}
+	})
+
+	await ack();
+	//TODO: Make it work
+});
+
 
 slack.message(async ({ message, client }) => {
 	if (message.subtype) return;
 	if(message.channel !== ONLY_ONCE_CHANNEL) return;
-
-	const possibleMessage = await Message.findOne({where:{content:message.text}});
-
-	if (!possibleMessage){
-		await Message.create({ts:message.ts,userId:message.user,content:message.text});
-	}
-
 	const { user, text, channel } = message;
 	if (!text) return;
 
-	if(text.length > 300){
+	const u = await User.findOne({where:{id:user}});
+
+
+	const message_thread = message.thread_ts || message.ts;
+
+	if(!u) {
+		slackLog(`CRITICAL ERRROR User ${user} not found in database`);
 		client.chat.postMessage({
 			channel: ONLY_ONCE_CHANNEL,
-			text: `${user} has been banned for exceeding the 300 character limit!!! Good Game.`,
-			thread_ts: message.ts
+			text: `CRITICAL ERRROR User ${user} not found in database`
 		})
-
-		client.chat.postMessage({
-			channel: user,
-			text: `You have been banned from the Only Once channel for exceeding the 300 character limit. Good Game.`,
-		})
-
-		await banUser(user);
 		return;
 	}
+
+	let msg = await Message.findOne({where:{content:message.text}});
+
+	if(msg){
+		u.update({banned:true});
+		slackLog(`User ${user} has been banned for sending the same message as someone else before.`);
+		client.chat.postMessage({
+			channel: ONLY_ONCE_CHANNEL,
+			text: `${user} has been banned for sending the same message as someone else before.`,
+			thread_ts: message_thread
+		})
+		
+		client.chat.postMessage({
+			channel: user,
+			text: `You have been banned from the Only Once channel for sending the same message as someone else before.`,
+		})
+
+		banUser(user);
+
+	} else {
+		msg = await Message.create({ts:message.ts,userId:message.user,content:message.text});
+		msg.save().catch(slackLog);
+
+		if(text.length > 300){
+			client.chat.postMessage({
+				channel: ONLY_ONCE_CHANNEL,
+				text: `${user} has been banned for exceeding the 300 character limit!!! Good Game.`,
+				thread_ts: message.ts
+			})
+
+			client.chat.postMessage({
+				channel: user,
+				text: `You have been banned from the Only Once channel for exceeding the 300 character limit. Good Game.`,
+			})
+
+			await banUser(user);
+			return;
+		}
+
+		u.increment("messageCount");
+		u.save().catch(slackLog);
+	}
+
+
 
 
 })
@@ -318,15 +382,26 @@ function parseUser(user:String /* <@ID|username>*/): ParsedSlackUser | null {
 }
 
 async function banUser(user:string):Promise<boolean> {
-	const u = await User.findOne({where:{id:user}});
-	if (!u) return false;
-	await u.update({banned:true});
 
-	await slack.client.conversations.kick({
-		channel: ONLY_ONCE_CHANNEL,
-		user: user
+	try {
+		await slack.client.conversations.kick({
+			channel: ONLY_ONCE_CHANNEL,
+			user: user
+		})
+		return true;
+	} catch {
+		return false;	
+	}
+}
+
+function slackLog(content:string) {
+	
+	slack.client.chat.postMessage({
+		channel: ONLY_ONCE_META,
+		text: new String(content).substring(0, 1000)
 	})
-	return true;
+
+	
 }
 
 await slack.start();
